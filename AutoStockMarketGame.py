e20a2a4188cbbc9eca20820e8075bc331c7eb29c
sfmqur <sammaruska@gmail.com>
2020-03-05 10:55:52 -0600

    added Bot version of the game

---
+++ AutoStockMarketGame.py
@@ -0,0 +1,12 @@
+# This version of the game will run continuously, the computer will buy and sell stocks based on rules specified under
+#  the user-rules.txt datafile. The user-rules file will have to preexist in order for the program to run.
+
+# command line args: username
+
+import os
+import sys
+
+# check for rules file
+# if it exists, load user account
+
+
